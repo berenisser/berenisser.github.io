17ba92a72b294dd1e2e0e65ed5da427bc0c8575b
berenisser <berenisser@gmail.com>
2017-08-05 16:14:12 -0400

    moving logo set

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,9 +21,46 @@ $(document).ready(function(){
 			$('.mas-cajas').hide();
 		});;
 	}); */
+
+	// Static variables - That do not change while scrolling
+	var header = $(".nav-wrapper"),
+	    headerHeight = header.height(), // Get height of header
+	 	 	logo = $(".logo-bere"), // Get the logo
+	 	 	logoHeight = logo.height(), // Get logo height
+	 	 	scrollTo = 120; // Animation until scrolled to this point
+
+	// Scroll function
+	$(window).on("scroll", function() {
+	  // Dynamic variables - That do change while scrolling
+	  var yPos = $(this).scrollTop(), // Get the scroll Y-position
+	  	  yPer = (yPos / scrollTo); // Calculate percenage of scroll
+
+	  // If percentage is over 100, set to 100
+	  if (yPer > 1) {
+	    yPer = 1;
+	  }
+	  // Dynamic variables for the elements
+	  var logoPos = ( -1*(yPer*25)+25), // Calculate position of logo (starting from 50%)
+	    	logoSize = ((headerHeight*yPer)-(logoHeight*yPer)+logoHeight), // Calculate new size height for logo
+	    	headerPos = ((yPer * headerHeight) - headerHeight); // Calculate position of header (starting from minus the height of itself)
+
+	  // Change the top, left, transform and height of the logo
+	  logo.css({
+	    top: logoPos + "%",
+	    left: logoPos + "%",
+	    transform: "translate3d(-" + logoPos + "%,-" + logoPos + "%,0)",
+	    height: logoSize
+	  });
+	  // Change the transform and opacity of the header
+	  $('.nav-wrapper').css({
+	    //transform: "translate3d(0," + headerPos + "px,0)",
+			top: headerPos,
+	    	opacity: yPer
+	  });
+	});
 }); 
 
-//SVG path animation
+//SVG path animation for portfolio
 (function() {
 	function init() {
 		var speed = 250,
